7fdd2fe26d9f074660320980b5f96eac1bebca2b
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2021-07-04 00:58:36 -0500

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,1 +1,14 @@
-alert "bien";
+var alturaInput = document.getElementById("altura");
+var pesoInput = document.getElementById("peso");
+var boton = document.getElementById("botonenviar")
+boton.addEventListener("click",calcularedad);
+
+
+
+function calcularedad()
+{
+ var valorAltura = parseInt(alturaInput.value);
+ var valorPeso = parseInt(pesoInput.value);
+ console.log(texto);
+ document.write("tu medida es de <strong>" + (valorAltura*valorPeso) + "</strong> kilos");
+}
